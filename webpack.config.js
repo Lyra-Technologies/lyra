ebe1316bf220c31ae1e634d66af9e8badc06a5f3
Nel Malikova <gunelmalikova@gmail.com>
2019-04-17 15:13:27 -0400

    configure webpack, set up basic extension
    Co-authored-by: tayvon <tayvonwright@gmail.com>
    Co-authored-by: nel <gunelmalikova@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './browser'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
