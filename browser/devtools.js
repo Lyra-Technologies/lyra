8b5d14d55cb06909251139a0b8e20438e0c6634a
Nel Malikova <gunelmalikova@gmail.com>
2019-04-18 12:05:56 -0400

    updating local repo

--- browser/devtools.js
+++ browser/devtools.js
@@ -1,38 +1,38 @@
 function show() {
-  console.log("panel showing");
+  console.log('panel showing');
 }
 
 function hide() {
-  console.log("panel hiding");
+  console.log('panel hiding');
 }
 
 chrome.devtools.panels.create(
   'Apollo state visualizer',
   null,
   'devtools.html',
-  function (panel){
+  function(panel) {
     panel.onShown.addListener(show);
     panel.onHidden.addListener(hide);
-}
+  }
 );
-  // ,devtoolsPanel => {
-  //   const backgroundPageConnection = chrome.runtime.connect({
-  //     name: 'devtools'
-  //   });
+// ,devtoolsPanel => {
+//   const backgroundPageConnection = chrome.runtime.connect({
+//     name: 'devtools'
+//   });
 
-    // devtoolsPanel.onShown.addListener(function tmp(panelWindow) {
-    //   // this only runs once
-    //   devtoolsPanel.onShown.removeListener(tmp);
+// devtoolsPanel.onShown.addListener(function tmp(panelWindow) {
+//   // this only runs once
+//   devtoolsPanel.onShown.removeListener(tmp);
 
-    //   const windowP = panelWindow;
-    //   windowP.backgroundPort = backgroundPageConnection;
+//   const windowP = panelWindow;
+//   windowP.backgroundPort = backgroundPageConnection;
 
-    //   backgroundPageConnection.onMessage.addListener(message => {
-    //     // handle responses from the background page
-    //     // listens for `refresh_devtool` message from the background
-    //     // and sends it to the app via window.PostMessage()
-    //     if (message.action === 'refresh_devtool') windowP.postMessage(message);
-    //   });
-    // });
-  // }
+//   backgroundPageConnection.onMessage.addListener(message => {
+//     // handle responses from the background page
+//     // listens for `refresh_devtool` message from the background
+//     // and sends it to the app via window.PostMessage()
+//     if (message.action === 'refresh_devtool') windowP.postMessage(message);
+//   });
+// });
+// }
 // );
