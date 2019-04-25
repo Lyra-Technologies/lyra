a5512dd8a0b8d89587680ec1dcf994aadf10aa4c
Akira Sane <akirasane@Akiras-MacBook-Pro.local>
2019-04-25 14:36:41 -0400

    refactored tree comp to take in props, implemented routes to switch views, for now, using dummy data

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 // import Header from './components/Header';
 import MainContainer from './containers/MainContainer';
-
 class App extends Component {
   constructor(props) {
     super(props);
