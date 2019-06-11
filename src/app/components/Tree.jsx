df6498b1252bc52730cb32c4408611003213da7d
Nel Malikova <gunelmalikova@gmail.com>
2019-06-11 11:47:57 -0400

    lint, clean up, delete dead code

--- src/app/components/Tree.jsx
+++ src/app/components/Tree.jsx
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 const Tree = props => {
   const clickedLeaves = {};
 
-  let treeStyles = {
+  const treeStyles = {
     selectedCircle: `transition: 0.2s; fill: #E534AB; stroke: gray; r: 7;`,
     selectedText: `transition: 0.2s; color: #E534AB; font-size: 18px; font-weight: bold; pacity: 1;`,
     unselectedCircle: `transition: 0.2s; fill: #280B9F; stroke: gray; r: 5;`,
@@ -23,9 +23,9 @@ const Tree = props => {
       });
     }
 
-    const margin = { top: 20, right: 90, bottom: 30, left: 90 },
-      width = 800 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    const margin = { top: 20, right: 90, bottom: 30, left: 90 };
+    const width = 800 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     // appends a 'group' element to 'svg'
@@ -36,11 +36,11 @@ const Tree = props => {
       .attr('width', width + margin.right + margin.left)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    let i = 0,
-      duration = 750,
-      root;
+    let i = 0;
+    const duration = 750;
+    let root;
 
     // Assigns parent, children, height, depth
     root = d3.hierarchy(treeData, function(d) {
@@ -51,25 +51,23 @@ const Tree = props => {
 
     // declares a tree layout and assigns the size
     const treemap = d3.tree();
-    let levelsObj = treemap(root)
+    const levelsObj = treemap(root)
       .descendants()
       .reduce((acc, cur) => {
-        if (acc[cur.depth]) acc[cur.depth]++;
+        if (acc[cur.depth]) acc[cur.depth] += 1;
         else acc[cur.depth] = 1;
         return acc;
       }, {});
-    let [newWidth, newHeight] = Object.entries(levelsObj).reduce((acc, cur) => {
-      return [
-        acc[0] >= cur[0] ? acc[0] : cur[0],
-        acc[1] >= cur[1] ? acc[1] : cur[1],
-      ];
-    });
+    let [newWidth, newHeight] = Object.entries(levelsObj).reduce((acc, cur) => [
+      acc[0] >= cur[0] ? acc[0] : cur[0],
+      acc[1] >= cur[1] ? acc[1] : cur[1],
+    ]);
     newHeight =
       newHeight * 20 > window.innerHeight ? newHeight * 20 : window.innerHeight;
     newWidth =
       newWidth * 180 > window.innerWidth ? newWidth * 180 : window.innerWidth;
     treemap.size([newHeight, newWidth]);
-    let svgElement = document.querySelector('svg');
+    const svgElement = document.querySelector('svg');
     svgElement.setAttribute('height', newHeight + 200);
     svgElement.setAttribute('width', newWidth + 180);
 
@@ -95,8 +93,8 @@ const Tree = props => {
       const treeData = treemap(root);
 
       // Compute the new tree layout.
-      const nodes = treeData.descendants(),
-        links = treeData.descendants().slice(1);
+      const nodes = treeData.descendants();
+      const links = treeData.descendants().slice(1);
       // Normalize for fixed-depth and get maxWidth and maxHeight.
       nodes.forEach(d => {
         d.y = d.depth * 180;
@@ -105,18 +103,16 @@ const Tree = props => {
       // ****************** Nodes section ***************************
 
       // Update the nodes...
-      const node = svg.selectAll('g.node').data(nodes, d => {
-        return d.id || (d.id = ++i);
-      });
+      const node = svg
+        .selectAll('g.node')
+        .data(nodes, d => d.id || (d.id = ++i));
 
       // Enter any new modes at the parent's previous position.
       const nodeEnter = node
         .enter()
         .append('g')
         .attr('class', 'node')
-        .attr('transform', d => {
-          return `translate(${source.y0},${source.x0})`;
-        })
+        .attr('transform', d => `translate(${source.y0},${source.x0})`)
         .on('click', handleClick)
         .on('dblclick', handleLeafClick);
 
@@ -137,23 +133,16 @@ const Tree = props => {
       nodeEnter
         .append('text')
         .attr('dy', '.35em')
-        .attr('x', d => {
-          return d.children || d._children ? -13 : -13;
-        })
-        .attr('text-anchor', d => {
-          return d.children || d._children ? 'end' : 'end';
-        })
+        .attr('x', d => (d.children || d._children ? -13 : -13))
+        .attr('text-anchor', d => (d.children || d._children ? 'end' : 'end'))
         .text(d => {
-          let length = d.data.name.length;
+          const { length } = d.data.name;
           if (length < 15) return d.data.name;
-          else {
-            return (
-              d.data.name
-                .split('')
-                .slice(0, 13)
-                .join('') + '...'
-            );
-          }
+
+          return `${d.data.name
+            .split('')
+            .slice(0, 13)
+            .join('')}...`;
         })
         .attr('style', d => {
           if (!isSearching) return `opacity: 1; font-weight: normal`;
@@ -169,9 +158,7 @@ const Tree = props => {
       nodeUpdate
         .transition()
         .duration(duration)
-        .attr('transform', d => {
-          return 'translate(' + d.y + ',' + d.x + ')';
-        });
+        .attr('transform', d => `translate(${d.y},${d.x})`);
 
       // Update the node attributes and style
       nodeUpdate
@@ -192,9 +179,7 @@ const Tree = props => {
         .exit()
         .transition()
         .duration(duration)
-        .attr('transform', d => {
-          return 'translate(' + source.y + ',' + source.x + ')';
-        })
+        .attr('transform', d => `translate(${source.y},${source.x})`)
         .remove();
 
       // On exit reduce the node circles size to 0
@@ -206,9 +191,7 @@ const Tree = props => {
       // ****************** links section ***************************
 
       // Update the links...
-      const link = svg.selectAll('path.link').data(links, d => {
-        return d.id;
-      });
+      const link = svg.selectAll('path.link').data(links, d => d.id);
 
       // Enter any new links at the parent's previous position.
       const linkEnter = link
@@ -231,9 +214,7 @@ const Tree = props => {
       linkUpdate
         .transition()
         .duration(duration)
-        .attr('d', d => {
-          return diagonal(d, d.parent);
-        })
+        .attr('d', d => diagonal(d, d.parent))
         .style('opacity', 0.2)
         .style('fill', '#ffffff')
         .style('stroke', 'E534AB');
@@ -260,18 +241,16 @@ const Tree = props => {
 
       // resize svg and tree
       function recalculateSize() {
-        let levelsObj = treeData.descendants().reduce((acc, cur) => {
-          if (acc[cur.depth]) acc[cur.depth]++;
+        const levelsObj = treeData.descendants().reduce((acc, cur) => {
+          if (acc[cur.depth]) acc[cur.depth] += 1;
           else acc[cur.depth] = 1;
           return acc;
         }, {});
         let [newWidth, newHeight] = Object.entries(levelsObj).reduce(
-          (acc, cur) => {
-            return [
-              acc[0] >= cur[0] ? acc[0] : cur[0],
-              acc[1] >= cur[1] ? acc[1] : cur[1],
-            ];
-          },
+          (acc, cur) => [
+            acc[0] >= cur[0] ? acc[0] : cur[0],
+            acc[1] >= cur[1] ? acc[1] : cur[1],
+          ]
         );
         console.log(window.innerHeight, window.innerWidth);
         console.log(newHeight * 20, newWidth * 180);
@@ -284,14 +263,14 @@ const Tree = props => {
             ? newWidth * 180
             : window.innerWidth - 400;
         treemap.size([newHeight, newWidth]);
-        let svgElement = document.querySelector('svg');
+        const svgElement = document.querySelector('svg');
         svgElement.setAttribute('height', newHeight + 200);
         svgElement.setAttribute('width', newWidth + 180);
       }
 
       // Creates a curved (diagonal) path from parent to the child nodes
       function diagonal(s, d) {
-        let path = `M ${s.y} ${s.x}
+        const path = `M ${s.y} ${s.x}
               C ${(s.y + d.y) / 2} ${s.x},
                 ${(s.y + d.y) / 2} ${d.x},
                 ${d.y} ${d.x}`;
@@ -319,25 +298,25 @@ const Tree = props => {
       // grow and display modeless popup when mouse click
       function handleLeafClick(d) {
         const circles = document.querySelectorAll('circle');
-        let { id } = d;
+        const { id } = d;
         console.log(id, Object.keys(clickedLeaves));
-        for (let key in clickedLeaves) {
+        for (const key in clickedLeaves) {
           if (key !== id) {
-            let selectedNode = document.querySelector('#selectedNode');
+            const selectedNode = document.querySelector('#selectedNode');
             selectedNode.firstChild.setAttribute(
               'style',
-              treeStyles.unselectedCircle,
+              treeStyles.unselectedCircle
             );
             selectedNode.firstChild.nextSibling.setAttribute(
               'style',
-              treeStyles.unselectedText,
+              treeStyles.unselectedText
             );
             selectedNode.setAttribute('id', '');
             delete clickedLeaves[key];
             d3.selectAll('.modeless-popup').remove();
           }
         }
-        for (let circle of circles) {
+        for (const circle of circles) {
           if (d.x === circle.__data__.x && d.y === circle.__data__.y) {
             console.log('leaves:', clickedLeaves);
             if (!d.hasOwnProperty('clicked') || !d.clicked) {
@@ -365,7 +344,8 @@ const Tree = props => {
                       popupWidth += 9;
                       popupWidth *= 8;
                       return popupWidth;
-                    } else return 1e-6;
+                    }
+                    return 1e-6;
                   })
                   .attr('height', '50')
                   .attr('rx', '10')
@@ -438,7 +418,7 @@ const Tree = props => {
               circle.setAttribute('style', treeStyles.unselectedCircle);
               circle.nextSibling.setAttribute(
                 'style',
-                treeStyles.unselectedText,
+                treeStyles.unselectedText
               );
               // eliminates modeless popup
               d3.selectAll('.modeless-popup').remove();
